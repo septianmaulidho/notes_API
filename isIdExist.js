0971d223be5b8a17e242e61d9b53073af777dc0d
septianmaulidho <72146052+septianmaulidho@users.noreply.github.com>
2020-10-14 19:44:47 +0700

    Delete isIdExist.js

--- isIdExist.js
+++
@@ -1,8 +0,0 @@
-function isIdExist(dbTemp, id) {
-    const index = dbTemp.findIndex((element) => {
-        const matchResult = element.id == id
-        return matchResult
-    })
-    return index >= 0
-}
-module.exports = isIdExist
